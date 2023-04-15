e7840f15de91a86bbe1b7010b81a7d5c453698b4
coderpy4 _ <coderpy4@proton.me>
2023-04-15 08:39:29 +0000

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,19 +1,15 @@
 a = list(map(float, input().split()))
-def checkin(x):
-    if abs(x) < 10**9:
-        x = bool(x)
-        return True
-    else:
-        return False
 
-if all(map(checkin, a)) == True:
-    if a[0] == a[3] and a[1] == a[4] and a[2] == a[5]: # infinity
-        print(-1)
-    elif a[2]+a[5] == abs(a[0]-a[3]): # 1
-        print(1)
-    elif a[2]+a[5] > abs(a[0]-a[3]): # 2
-        print(2)
-    elif a[2]+a[5] < abs(a[0]-a[3]): # 0
-        print(0)
+distance = ((a[3] - a[0])**2 + (a[4] - a[1])**2)**0.5
+
+if distance > a[2] + a[5]:
+    print(0)
+elif distance == 0 and a[5] == a[2]:
+    print(-1)
 else:
-    print("nope")
+    if distance == a[5] + a[2] or distance == abs(a[5] - a[2]):
+        print(1)
+    elif distance < abs(a[5] - a[2]):
+        print(0)
+    else:
+        print(2)
