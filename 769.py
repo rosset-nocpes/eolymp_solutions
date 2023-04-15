3e3db2a4fcdc647004d6b4bddbff6c8b1a4d9ddd
coderpy4 <coderpy4@proton.me>
2023-04-15 21:24:13 +0300

    Create soulution for pr. 769
    Signed-off-by: coderpy4 <coderpy4@proton.me>

--- 769.py
+++ 769.py
@@ -1,5 +1,21 @@
-class rectangle:
-    x1, y1 = input().split()
-    x2, y2 = input().split()
-    x3, y3 = input().split()
-    x4, y4 = 0, 0
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+x3, y3 = map(int, input().split())
+x4, y4 = 0, 0
+
+if x1 == x2:
+    x4 = x3
+elif x1 == x3:
+    x4 = x2
+else:
+    x4 = x1
+
+if y1 == y2:
+    y4 = y3
+elif y1 == y3:
+    y4 = y2
+else:
+    y4 = y1
+
+print(x4, y4)
+
